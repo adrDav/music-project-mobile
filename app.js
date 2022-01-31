5501873bd5e577e90bab103727bc1a72e7e4d3bd
adrDav <61762020+adrDav@users.noreply.github.com>
2022-01-31 15:59:37 -0700

    updated app.js - audioPlayer post request fix needed.

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ app.listen(8000, () => console.log('listening at 8000'));
 app.use(express.static('public'));
 app.use(express.json());
 
+temp_data = {}
+
 app.post('/', (request, response) => {
-    console.log('I got a request!');
     console.log(request.body);
     const data = request.body;
-    response.json({
-        status: 'success',
-        latitude: data.lat,
-        longitude: data.lon
-    });
+    const json_data = JSON.stringify(data);
+    response.send(json_data);
+    temp_data = json_data;
 });
+
+app.post('/audioPlayer.js', (request, response) => {
+    response.send(temp_data);
+});
+
+
+
+
