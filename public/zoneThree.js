6e9651498f06f54298218d332433ec1594b94771
adrDav <61762020+adrDav@users.noreply.github.com>
2022-02-25 14:26:59 -0700

    added zone two and three

---
+++ public/zoneThree.js
@@ -0,0 +1,31 @@
+class zoneThree {
+    constructor(){    
+        this.first_lat = 31.770973; 
+        this.first_lng = -106.505293;
+
+        this.second_lat = 31.770883;
+        this.second_lng = -106.504641;
+
+        this.third_lat = 31.770587;
+        this.third_lng = -106.505225;
+    }
+    
+    get get_first_lat(){
+        return this.first_lat;
+    }
+    get get_first_lng(){
+        return this.first_lng;
+    }
+    get get_second_lat(){
+        return this.second_lat;
+    }
+    get get_second_lng(){
+        return this.second_lng;
+    }
+    get get_third_lat(){
+        return this.third_lat;
+    }
+    get get_third_lng(){
+        return this.third_lng;
+    }
+}
