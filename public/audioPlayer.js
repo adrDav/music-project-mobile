4e48f6876e2d6b242786e8e2487f4d304658413e
CuldesacDude <kevin12518176@gmail.com>
2022-02-27 14:31:21 -0700

    asdf

--- public/audioPlayer.js
+++ public/audioPlayer.js
@@ -23,108 +23,108 @@ https://github.com/mdn/webaudio-examples/blob/master/step-sequencer/index.html
 yet another tutorial on WAAPI
 https://k6.io/blog/webaudio_explained/
 */
-//window.addEventListener("click", console.log("hey"));
-function connectCtx(){
-window.AudioContext ? new AudioContext() : webkitAudioContext();
-ctx = new AudioContext();
-ctx1 = new AudioContext();
-ctx2 = new AudioContext();
-ctx3 = new AudioContext();
-ctx4 = new AudioContext();
-ctx5 = new AudioContext();
-ctx6 = new AudioContext();
-ctx7 = new AudioContext();
-ctx8 = new AudioContext();
-ctx9 = new AudioContext();
+window.addEventListener("click", (event)=> {
+    window.AudioContext ? new AudioContext() : webkitAudioContext();
+    ctx = new AudioContext();
+    ctx1 = new AudioContext();
+    ctx2 = new AudioContext();
+    ctx3 = new AudioContext();
+    ctx4 = new AudioContext();
+    ctx5 = new AudioContext();
+    ctx6 = new AudioContext();
+    ctx7 = new AudioContext();
+    ctx8 = new AudioContext();
+    ctx9 = new AudioContext();
 
-const audio = document.getElementById("audioContainer1");
-const audio1 = document.getElementById("audioContainer2");
-const audio2 = document.getElementById("audioContainer3");
-const audio3 = document.getElementById("audioContainer4");
-const audio4 = document.getElementById("audioContainer5");
-const audio5 = document.getElementById("audioContainer6");
-const audio6 = document.getElementById("audioContainer7");
-const audio7 = document.getElementById("audioContainer8");
-const audio8 = document.getElementById("audioContainer9");
-const audio9 = document.getElementById("audioContainer10");
+    const audio = document.getElementById("audioContainer1");
+    const audio1 = document.getElementById("audioContainer2");
+    const audio2 = document.getElementById("audioContainer3");
+    const audio3 = document.getElementById("audioContainer4");
+    const audio4 = document.getElementById("audioContainer5");
+    const audio5 = document.getElementById("audioContainer6");
+    const audio6 = document.getElementById("audioContainer7");
+    const audio7 = document.getElementById("audioContainer8");
+    const audio8 = document.getElementById("audioContainer9");
+    const audio9 = document.getElementById("audioContainer10");
 
-var audioSource1 = ctx.createMediaElementSource(audio);
-var audioSource2 = ctx1.createMediaElementSource(audio1);
-var audioSource3 = ctx2.createMediaElementSource(audio2);
-var audioSource4 = ctx3.createMediaElementSource(audio3);
-var audioSource5 = ctx4.createMediaElementSource(audio4);
-var audioSource6 = ctx5.createMediaElementSource(audio5);
-var audioSource7 = ctx6.createMediaElementSource(audio6);
-var audioSource8 = ctx7.createMediaElementSource(audio7);
-var audioSource9 = ctx8.createMediaElementSource(audio8);
-var audioSource10 = ctx9.createMediaElementSource(audio9);
+    var audioSource1 = ctx.createMediaElementSource(audio);
+    var audioSource2 = ctx1.createMediaElementSource(audio1);
+    var audioSource3 = ctx2.createMediaElementSource(audio2);
+    var audioSource4 = ctx3.createMediaElementSource(audio3);
+    var audioSource5 = ctx4.createMediaElementSource(audio4);
+    var audioSource6 = ctx5.createMediaElementSource(audio5);
+    var audioSource7 = ctx6.createMediaElementSource(audio6);
+    var audioSource8 = ctx7.createMediaElementSource(audio7);
+    var audioSource9 = ctx8.createMediaElementSource(audio8);
+    var audioSource10 = ctx9.createMediaElementSource(audio9);
 
-// lowshelf nodes
+    // lowshelf nodes
 
-bassFilter = ctx.createBiquadFilter();
-bassFilter.type = "lowshelf";
-bassFilter.frequency.value = 200; 
+    bassFilter = ctx.createBiquadFilter();
+    bassFilter.type = "lowshelf";
+    bassFilter.frequency.value = 200; 
 
-bassFilter1 = ctx1.createBiquadFilter();
-bassFilter1.type = "lowshelf";
-bassFilter1.frequency.value = 200; 
+    bassFilter1 = ctx1.createBiquadFilter();
+    bassFilter1.type = "lowshelf";
+    bassFilter1.frequency.value = 200; 
 
-bassFilter2 = ctx2.createBiquadFilter();
-bassFilter2.type = "lowshelf";
-bassFilter2.frequency.value = 200;
+    bassFilter2 = ctx2.createBiquadFilter();
+    bassFilter2.type = "lowshelf";
+    bassFilter2.frequency.value = 200;
 
-bassFilter3 = ctx3.createBiquadFilter();
-bassFilter3.type = "lowshelf";
-bassFilter3.frequency.value = 200; 
+    bassFilter3 = ctx3.createBiquadFilter();
+    bassFilter3.type = "lowshelf";
+    bassFilter3.frequency.value = 200; 
 
-bassFilter4 = ctx4.createBiquadFilter();
-bassFilter4.type = "lowshelf";
-bassFilter4.frequency.value = 200;
+    bassFilter4 = ctx4.createBiquadFilter();
+    bassFilter4.type = "lowshelf";
+    bassFilter4.frequency.value = 200;
 
-// highself nodes
+    // highself nodes
 
-trebleFilter = ctx.createBiquadFilter();
-trebleFilter.type = "highshelf"; 
-trebleFilter.frequency.value = 2000;
+    trebleFilter = ctx.createBiquadFilter();
+    trebleFilter.type = "highshelf"; 
+    trebleFilter.frequency.value = 2000;
 
-trebleFilter1 = ctx1.createBiquadFilter();
-trebleFilter1.type = "highshelf"; 
-trebleFilter1.frequency.value = 2000;
+    trebleFilter1 = ctx1.createBiquadFilter();
+    trebleFilter1.type = "highshelf"; 
+    trebleFilter1.frequency.value = 2000;
 
-trebleFilter2 = ctx2.createBiquadFilter();
-trebleFilter2.type = "highshelf"; 
-trebleFilter2.frequency.value = 2000;
+    trebleFilter2 = ctx2.createBiquadFilter();
+    trebleFilter2.type = "highshelf"; 
+    trebleFilter2.frequency.value = 2000;
 
-trebleFilter3 = ctx3.createBiquadFilter();
-trebleFilter3.type = "highshelf"; 
-trebleFilter3.frequency.value = 2000;
+    trebleFilter3 = ctx3.createBiquadFilter();
+    trebleFilter3.type = "highshelf"; 
+    trebleFilter3.frequency.value = 2000;
 
-trebleFilter4 = ctx4.createBiquadFilter();
-trebleFilter4.type = "highshelf"; 
-trebleFilter4.frequency.value = 2000;
+    trebleFilter4 = ctx4.createBiquadFilter();
+    trebleFilter4.type = "highshelf"; 
+    trebleFilter4.frequency.value = 2000;
 
-//connected nodes
-audioSource1.connect(bassFilter);
-bassFilter.connect(trebleFilter); 
-trebleFilter.connect(ctx.destination);
+    //connected nodes
+    audioSource1.connect(bassFilter);
+    bassFilter.connect(trebleFilter); 
+    trebleFilter.connect(ctx.destination);
 
-audioSource2.connect(bassFilter1);
-bassFilter1.connect(trebleFilter1); 
-trebleFilter1.connect(ctx1.destination);
+    audioSource2.connect(bassFilter1);
+    bassFilter1.connect(trebleFilter1); 
+    trebleFilter1.connect(ctx1.destination);
 
-audioSource3.connect(bassFilter2);
-bassFilter2.connect(trebleFilter2); 
-trebleFilter2.connect(ctx2.destination);
+    audioSource3.connect(bassFilter2);
+    bassFilter2.connect(trebleFilter2); 
+    trebleFilter2.connect(ctx2.destination);
 
-audioSource4.connect(bassFilter3);
-bassFilter3.connect(trebleFilter3); 
-trebleFilter3.connect(ctx3.destination);
+    audioSource4.connect(bassFilter3);
+    bassFilter3.connect(trebleFilter3); 
+    trebleFilter3.connect(ctx3.destination);
 
-audioSource5.connect(bassFilter4);
-bassFilter4.connect(trebleFilter4); 
-trebleFilter4.connect(ctx4.destination);
+    audioSource5.connect(bassFilter4);
+    bassFilter4.connect(trebleFilter4); 
+    trebleFilter4.connect(ctx4.destination);
 
-}
+
+})
 //play and loop all audios
 
 function playAudio(){
