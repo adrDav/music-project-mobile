1e3d49219a4babd868a51435da34226849e6e453
adrDav <61762020+adrDav@users.noreply.github.com>
2022-03-02 15:41:49 -0700

    added zones 6 - 9

---
+++ public/zoneEight.js
@@ -0,0 +1,31 @@
+class zoneEight {
+    constructor(){    
+        this.first_lat = 31.771290; 
+        this.first_lng = -106.504835;
+
+        this.second_lat = 31.771269;
+        this.second_lng = -106.504464;
+
+        this.third_lat = 31.770887;
+        this.third_lng = -106.504642;
+    }
+    
+    get get_first_lat(){
+        return this.first_lat;
+    }
+    get get_first_lng(){
+        return this.first_lng;
+    }
+    get get_second_lat(){
+        return this.second_lat;
+    }
+    get get_second_lng(){
+        return this.second_lng;
+    }
+    get get_third_lat(){
+        return this.third_lat;
+    }
+    get get_third_lng(){
+        return this.third_lng;
+    }
+}
---
+++ public/zoneNine.js
@@ -0,0 +1,39 @@
+class zoneFour {
+    constructor(){
+        this.first_lat = 31.770863;
+        this.first_lng = -106.504678;
+
+        this.second_lat = 31.770931;
+        this.second_lng = --106.504596;
+
+        this.third_lat = 31.770857;
+        this.third_lng = -106.504508;
+
+        this.fourth_lat = 31.770803;
+        this.fourth_lng = -106.504609;
+    }
+    get get_first_lat(){
+        return this.first_lat;
+    }
+    get get_first_lng(){
+        return this.first_lng;
+    }
+    get get_second_lat(){
+        return this.second_lat;
+    }
+    get get_second_lng(){
+        return this.second_lng;
+    }
+    get get_third_lat(){
+        return this.third_lat;
+    }
+    get get_third_lng(){
+        return this.third_lng;
+    }
+    get get_fourth_lat(){
+        return this.fourth_lat;
+    }
+    get get_fourth_lng(){
+        return this.fourth_lng;
+    }
+}
