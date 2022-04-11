23902f4b7d6cc758dd7e439c64c0cfecb1970514
CuldesacDude <kevin12518176@gmail.com>
2022-04-11 12:04:37 -0600

    i think i made some changes to zone 8 ?

---
+++ public/OfficeZone.js
@@ -0,0 +1,31 @@
+class zoneEight {
+    constructor(){    
+        this.first_lat = 31.767935; 
+        this.first_lng = -106.502453;
+
+        this.second_lat = 31.767488;
+        this.second_lng = -106.502066;
+
+        this.third_lat = 31.767999;
+        this.third_lng = -106.501852;
+    }
+    
+    get get_first_lat(){
+        return this.first_lat;
+    }
+    get get_first_lng(){
+        return this.first_lng;
+    }
+    get get_second_lat(){
+        return this.second_lat;
+    }
+    get get_second_lng(){
+        return this.second_lng;
+    }
+    get get_third_lat(){
+        return this.third_lat;
+    }
+    get get_third_lng(){
+        return this.third_lng;
+    }
+}
