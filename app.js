b9f60c911c5f80c790bcf5e0f8602e3afd455222
CuldesacDude <52584830+CuldesacDude@users.noreply.github.com>
2022-04-15 12:58:35 -0600

    update to server

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ wsServer.on("request", request => {
   const connection = request.accept(null, request.origin);
   connection.on("open", () => console.log("opened!"))
   connection.on("close", () => { 
-
+    console.log("closed!")
   })
   connection.on("message", message => {
   
@@ -46,6 +46,8 @@ wsServer.on("request", request => {
     if(result.method === "close"){
       const clientID = result.clientID;
       delete clients[clientID];
+      delete zone1[clientID];
+      delete zone2[clientID];
       
       
     }
@@ -62,10 +64,22 @@ wsServer.on("request", request => {
       
     }
 
+    console.log("count of users in server : ");
     var keys = Object.keys(clients);
-    console.log(Object.keys(clients).length);
     keys.forEach(key=>{
       console.log(key + '|' + clients[key]);
+    });
+
+    console.log("users in zone 1 :");
+    var keys = Object.keys(zone1);
+    keys.forEach(key=>{
+      console.log(key + '|' + zone1[key]);
+    });
+
+    console.log("users in zone 2 :");
+    var keys = Object.keys(zone2);
+    keys.forEach(key=>{
+      console.log(key + '|' + zone2[key]);
     });
 
 })
