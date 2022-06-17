a344bcd110f4cf850257ba16f52a9f5610a384d1
CuldesacDude <52584830+CuldesacDude@users.noreply.github.com>
2022-06-17 02:23:45 -0600

    just a new update

--- app.js
+++ app.js
@@ -16,9 +16,63 @@ serv.listen(2000);
 console.log('server started');
 
 var SOCKET_LIST = {};
-var CLIENT_LIST = {};
-var ZONE_LIST = [0,0,0,0,0,1,1,1];
 
+var zone1 = {};
+var zone2 = {};
+var zone3 = {};
+var zone4 = {};
+var zone5 = {};
+var zone6 = {};
+var zone7 = {};
+var zone8 = {};
+var zone9 = {};
+var zone10 = {};
+
+var ZONE_LIST = {
+  zone1,zone2,zone3,zone4,zone5,
+  zone6,zone7,zone8,zone9,zone10
+};
+
+var allZones = [[//zone 1
+                [31.771011, -106.505353],[31.771572, -106.50448],
+                [31.770785, -106.5033789],[31.769928, -106.505274]
+                ],
+                [//zone 2
+                  [31.770973, -106.505293],[31.771267, -106.50482],
+                  [31.770883, -106.504641]
+                ],
+                [//zone 3
+                  [31.770973, -106.505293],[31.770883, -106.504641],
+                  [31.770587, -106.505225]
+                ],
+                [//zone 4
+                  [31.770465, -106.505247],[31.770530, -106.504931],
+                  [31.770342, -106.504719],[31.770162, -106.50518]
+                ],
+                [//zone 5
+                  [31.770883, -106.504641],[31.770732, -106.503878],
+                  [31.770481, -106.504262]
+                ],
+                [//zone 6
+                  [31.770982, -106.505153],[31.771264, -106.504611],
+                  [31.770737, -106.504089],[31.770392, -106.504567]
+                ],
+                [//zone 7
+                  [31.770957, -106.50495],[31.771119, -106.504621],
+                  [31.770771, -106.504239],[31.770562, -106.504508]
+                ],
+                [//zone 8
+                  [31.771290, -106.504835],[31.771269, -106.504464],
+                  [31.770887, -106.504642]
+                ],
+                [//zone 9
+                  [31.770863, -106.504678],[31.770931, -106.504596],
+                  [31.770857, -106.504508],[31.770803, -106.504609]
+                ],
+                [//zone 10
+                  [31.7810, -106.489],[31.7790, -106.4875],
+                  [31.7820, -106.4867]
+                ]];
 
 
 var io = require('socket.io')(serv,{});
@@ -30,27 +84,73 @@ io.sockets.on('connection', function(socket){
   const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
   //client ID gets assigned
   socket.id = guid();
-  socket.zoneIn = [0,0,0,0,0,0,0,0];
+  
   socket.coords = [0,0];
+  
   SOCKET_LIST[socket.id]=socket;
+  
+  
+
   console.log('socket connection confirmed');
   
   socket.on('disconnect', function(){
     console.log('user disconnected ID:'+socket.id);
+    delete zone1[socket.id];
+    delete zone2[socket.id];
+    delete zone3[socket.id];
+    delete zone4[socket.id];
+    delete zone5[socket.id];
+    delete zone6[socket.id];
+    delete zone7[socket.id];
+    delete zone8[socket.id];
+    delete zone9[socket.id];
+    delete zone10[socket.id];
     delete SOCKET_LIST[socket.id];
   });
 
   socket.on('userCoords', function(data){
     socket.coords = [data.lat,data.lng]
     console.log(socket.coords);
-    console.log(inside(socket.coords,verOfficeZone));
+    for(var i in allZones){
+    
+      if(inside(socket.coords, allZones[i])=== true){
+        if(i==0){
+          zone1[socket.id]=1;
+        }
+        if(i==1){
+          zone2[socket.id]=2;
+        }
+        if(i==2){
+          zone3[socket.id]=3;
+        }
+        if(i==3){
+          zone4[socket.id]=4;
+        }  
+        if(i==4){
+          zone5[socket.id]=5;
+        }
+        if(i==5){
+          zone6[socket.id]=6;
+        }
+        if(i==6){
+          zone7[socket.id]=7;
+        }
+        if(i==7){
+          zone8[socket.id]=8;
+        }
+        if(i==8){
+          zone9[socket.id]=9;
+        }
+        if(i==9){
+          zone10[socket.id]=10;
+        }
+        for(prop in ZONE_LIST){
+          console.log(ZONE_LIST[prop]);
+        }
+      }
+    }
   });
-
 });
-
-
-
-
 
 setInterval(function(){
   var pack = [];
@@ -62,14 +162,11 @@ setInterval(function(){
       coord:socket.coords
     })
   }
-
-
   //emit to each client
   for(var i in SOCKET_LIST){
     var socket = SOCKET_LIST[i];
     socket.emit('newPos', pack);
   }
-
 //new location every 5 secs
 },5000);
 
@@ -95,45 +192,3 @@ function inside(point, vs) {
 
 }
 
-var verZoneOne = [
-  [31.771011, -106.505353],[31.771572, -106.50448],
-  [31.770785, -106.5033789],[31.769928, -106.505274]
-];
-var verZoneTwo = [
-  [31.770973, -106.505293],[31.771267, -106.50482],
-  [31.770883, -106.504641]
-];
-var verZoneThree = [
-  [31.770973, -106.505293],[31.770883, -106.504641],
-  [31.770587, -106.505225]
-];
-var verZoneFour = [
-  [31.770465, -106.505247],[31.770530, -106.504931],
-  [31.770342, -106.504719],[31.770162, -106.50518]
-];
-var verZoneFive = [
-  [31.770883, -106.504641],[31.770732, -106.503878],
-  [31.770481, -106.504262]
-  //[zone_five.get_fourth_lat, zone_five.get_fourth_lng]
-];
-var verZoneSix = [
-  [31.770982, -106.505153],[31.771264, -106.504611],
-  [31.770737, -106.504089],[31.770392, -106.504567]
-];
-var verZoneSeven = [
-  [31.770957, -106.50495],[31.771119, -106.504621],
-  [31.770771, -106.504239],[31.770562, -106.504508]
-];
-var verZoneEight = [
-  [31.771290, -106.504835],[31.771269, -106.504464],
-  [31.770887, -106.504642]
-  //[zone_eight.get_fourth_lat, zone_eight.get_fourth_lng]
-];
-var verZoneNine = [
-  [31.770863, -106.504678],[31.770931, -106.504596],
-  [31.770857, -106.504508],[31.770803, -106.504609]
-];
-var verOfficeZone = [
-  [31.7810, -106.489],[31.7790, -106.4875],
-  [31.7820, -106.4867]
-];
